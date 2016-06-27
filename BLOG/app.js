d202f7dde7b1e8e2adfa79f49f41ba03105ed9a2
food <864741099@qq.com>
2016-06-28 02:49:00 +0800

    web_eg_1

--- BLOG/app.js
+++ BLOG/app.js
@@ -4,9 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs      = require('express-handlebars');
+
+// var mongoose = require('mongoose');
+// var config = require('./config');
+// var dbHelper = require('./db/dbHelper')/
+
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var admin = require('./routes/admin');
 
 var app = express();
 
@@ -14,8 +20,23 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//配置hbs基础模板和分块模板
+var hbs = exphbs.create({
+  partialsDir: 'views/partials',
+  layoutsDir: "views/layouts/",
+  defaultLayout: 'main',
+  extname: '.hbs'
+});
+app.engine('hbs', hbs.engine);
+
+// //连接数据库
+// try {
+//     mongoose.connect(config.db.url);
+// } catch (error) {
+//     console.log(error);
+// }
+
+//配置解析器，静态资源映射
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +44,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/admin', admin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,7 +53,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
 
 // development error handler
 // will print stacktrace
--- BLOG/routes/index.js
+++ BLOG/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { title: 'Express' , layout:'main'} );
 });
+router.get('/login', function(req,res,next) {
+    res.render('login',{ title: 'Express', layout:'lg'});
+});
+
+
 
 module.exports = router;
