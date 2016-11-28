fb402263063eafb4023409c84feda60e3f9b99a6
food <864741099@qq.com>
2016-11-28 18:53:54 +0800

    calculator last version

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,8 +1,9 @@
 var start = true; 
 var doubleOperation = false; //是不是将要输入第二个操作符
-var doubleEqual = false;   //是不是第二次按等号
+//var doubleEqual = false;   //是不是第二次按等号
 var lastOperation = '';
-
+var preKey = "";
+var doubleEqual = false;
 
 //把选择的数字或操作符添加入运算公式
 function  show(obj) {
@@ -23,7 +24,9 @@ function clearMsg(){
 	output.value = '0';
 	start = true;
 	doubleOperation = false;
+	doubleEqual = false;
 	lastOperation = '';
+	preKey = "";
 }
 
 //正负转换
@@ -59,9 +62,75 @@ function percent(){
 
 
 
+
+
+
+// 计算结果
+function getResult() {
+	var output = document.getElementById('output');
+	// alert(preKey);
+	if(preKey!== "="){         //如果是第一次按等号
+		if (doubleOperation) {   //如果最后一位是符号位   //检查最后是否是符号，保存相应的lastOperation 
+
+				// }else{      //6+=的情况，最后一位是符号位
+				var itself = output.value.slice(0,output.value.length-1);
+				lastOperation = output.value.slice(-1) + itself;
+				output.value = eval(output.value.slice(0,-1)+ lastOperation);
+				}else{
+					if (output.value.slice(-1)!=="+"&&output.value.slice(-1)!=="-"&&output.value.slice(-1)!=="*"&&output.value.slice(-1)!=="/") {
+					//0+6的情况，最后一位不是符号位
+					var i;
+					for(i = 0; i < output.value.length; i++){
+						if (output.value.slice(i,i+1)==preKey) {
+							lastOperation = output.value.slice(i);
+							// alert(lastOperation);
+						}
+					};
+					output.value = eval(output.value); 
+				}
+				}
+		preKey = "=";
+		}else if(preKey == "="){     //如果是第二次按等号
+			//读取并保存最后一个操作
+			var i;
+			for (i = output.value.length - 1; i >= 0 ; i --) {   //如果是第二次取等号，就不用重新保存lastOperation变量
+				console.log(output.value.slice(i,i+1));
+				if (!doubleEqual) {
+					if(output.value.slice(i,i+1) == "+"||output.value.slice(i,i+1) == "-"||output.value.slice(i,i+1) == "*"||output.value.slice(i,i+1) == "/"){
+						// alert("找到一个+");
+						lastOperation = output.value.slice(i);
+						console.log(lastOperation);
+						break;
+					}
+				}
+			}
+			doubleEqual = true;
+ 			output.value += lastOperation;
+ 			console.log(output.value);
+ 		}
+ 		output.value = eval(output.value);
+ 		preKey = "=";
+}
+
+// 计算结果
+function resultTrriger() {
+	var output = document.getElementById('output');
+	if (doubleOperation) {   //检查最后是否是符号，是就删除一位
+		var output = output.value.slice(0,output.value.length-1);
+	}
+	output.value = eval(output.value);
+	doubleOperation = false;
+	if(output.value !== "0"){
+		start = false;
+	}
+}
+
 //加
 function plusNumber(obj){
-	if (start !== true) {
+	resultTrriger();
+	if (start !== true) {    //不是符号位开头
+		preKey = "+";
+		// if (preKey=="+") {alert("ok!");}
 		var output = document.getElementById('output');
 		var data = obj.value;
 		if (doubleOperation) {   //前一位是符号位,去掉一位，再加符号位
@@ -74,32 +143,71 @@ function plusNumber(obj){
 		};
 		output.value += data;
 		doubleOperation = true;
+	}else{      //如果以符号开头 -6 == 0-6
+		var data = obj.value;
+		var output = document.getElementById('output');
+		output.value += data;
+		doubleOperation = true;
+		start = false;
+		preKey = "+";
+	};
+}
+
+//原减
+function minusNumber(obj){
+	resultTrriger();
+	if (start !== true) {    //不是符号位开头
+		preKey = "-";
+		// if (preKey=="+") {alert("ok!");}
+		var output = document.getElementById('output');
+		var data = obj.value;
+		if (doubleOperation) {   //前一位是符号位,去掉一位，再加符号位
+			output.value = output.value.slice(0,-1);
+		}
+		//如果前面删去的是小数点，那还要再判断要不要删一位
+		var test = output.value.slice(-1);
+		if (test == "+" || test == "-" || test == "*" || test == "/") {
+			output.value = output.value.slice(0,-1);
+		};
+		output.value += data;
+		doubleOperation = true;
+	}else{      //如果以符号开头 -6 == 0-6
+		var data = obj.value;
+		var output = document.getElementById('output');
+		output.value += data;
+		doubleOperation = true;
+		start = false;
+		preKey = "+";
 	};
 }
 
 //减
-function minusNumber(obj){
-	if (start !== true) {
+function minusNumber2(obj){
+	resultTrriger();
+	if (start !== true) {    //如果不是符号开头
+		preKey = "-";
 		var output = document.getElementById('output');
 		var data = obj.value;
-		if (doubleOperation) {   //前一位是符号位,去掉一位，再加符号位
-			output.value = output.value.slice(0,-1);
-		}
-		//如果前面删去的是小数点，那还要再判断要不要删一位
-		var test = output.value.slice(-1);
-		if (test == "+" || test == "-" || test == "*" || test == "/") {
-			output.value = output.value.slice(0,-1);
-		};
 		output.value += data;
 		doubleOperation = true;
+	}else{      //如果以符号开头 -6 == 0-6
+		var data = obj.value;
+		var output = document.getElementById('output');
+		output.value += data;
+		doubleOperation = true;
+		start = false;
+		preKey = "-";
 	}
 }
 
 
 //乘
 function multiNumber(obj){
+	resultTrriger();
+	var output = document.getElementById('output');
 	if (start !== true) {
-		var output = document.getElementById('output');
+		preKey = "*";
+		
 		var data = obj.value;
 		if (doubleOperation) {   //前一位是符号位,去掉一位，再加符号位
 			output.value = output.value.slice(0,-1);
@@ -111,13 +219,22 @@ function multiNumber(obj){
 		};
 		output.value += data;
 		doubleOperation = true;
+	}else{      //如果以符号开头 -6 == 0-6
+		var data = obj.value;
+		var output = document.getElementById('output');
+		output.value += data;
+		doubleOperation = true;
+		start = false;
+		preKey = "*";
 	}
 }
 
 
 //除
 function divideNumber(obj){
+	resultTrriger();
 	if (start !== true) {
+		preKey = "/";
 		var output = document.getElementById('output');
 		var data = obj.value;
 		if (doubleOperation) {   //前一位是符号位,去掉一位，再加符号位
@@ -130,6 +247,13 @@ function divideNumber(obj){
 		};
 		output.value += data;
 		doubleOperation = true;
+	}else{      //如果以符号开头 -6 == 0-6
+		var data = obj.value;
+		var output = document.getElementById('output');
+		output.value += data;
+		doubleOperation = true;
+		start = false;
+		preKey = "/";
 	}
 }
 
@@ -147,49 +271,6 @@ function addPoint(obj){
 }
 
 
-
-// 计算结果
-function getResult() {
-	var output = document.getElementById('output');
-	if(!doubleEqual){         //如果是第一次按等号
-		if (doubleOperation) {   //检查最后是否是符号，是就删除一位
-			output.value = output.value.slice(0,-1);
-		}
-			//读取并保存最后一个操作
-			for (var i = output.value.length - 1; i >= 0; i --) {
-			console.log(output.value.slice(i,i+1));
-			if(output.value.slice(i,i+1) == "+"){
-				// alert("找到一个+");
-				lastOperation = output.value.slice(i);
-				console.log(lastOperation);
-				break;
-			}
-			if(output.value.slice(i,i+1) == "-"){
-				// alert("找到一个-");
-				lastOperation = output.value.slice(i);
-				console.log(lastOperation);
-				break;
-			}
-			if(output.value.slice(i,i+1) == "*"){
-				// alert("找到一个*");
-				lastOperation = output.value.slice(i);
-				console.log(lastOperation);
-				break;
-			}
-			if(output.value.slice(i,i+1) == "/"){
-				// alert("找到一个/");
-				lastOperation = output.value.slice(i);
-				console.log(lastOperation);
-				break;
-			}
-		};
-		doubleEqual = true;
- 		}else{       //如果不是第一次按等号
- 			output.value += lastOperation;
- 			console.log(output.value);
- 		}
- 		output.value = eval(output.value);
-}
 
 
 
